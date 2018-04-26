b9f411d1023f0f7150670ef08228be25c48a87ba
Tai Linh Du <linh9a0910@gmail.com>
2018-04-26 23:10:37 +0200

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace HelperLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+  
+}
